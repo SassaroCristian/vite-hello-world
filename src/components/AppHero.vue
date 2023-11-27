05ff813174d8d16d9af5a4b60968ba33cd96deb9
Zazzaro <sassaro01cristian@gmail.com>
2023-11-27 15:28:01 +0100

    completed exercise

---
+++ src/components/AppHero.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <img src="https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg" alt="">
+</template>
+
+<style scoped>
+</style>
